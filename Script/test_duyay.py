92fe09fb34fdaf4793b80dcb06f5e2afa091b2a1
niuqun1 <1103126307@qq.com>
2019-09-12 12:17:21 +0800

    更新缩进

--- Script/test_duyay.py
+++ Script/test_duyay.py
@@ -23,33 +23,37 @@ d.session("ai.zile.app")
 class Test_1:
     def setup_class(self):
         self.obj=search(d)
-        @allure.step(title="点击微信登陆")
-        def test_001(self):
-            self.obj.click_wx_lojin()
-        @allure.step(title="点击如何上课")
-        def test_002(self):
-            self.obj.click_who()
-            self.obj.click_who_add()
-        @allure.step(title="点击购买课程")
-        def test_007(self):
-            sleep(1)
-            self.obj.click_buy_coiu()
-        @allure.step(title="点击课堂")
-        def test_003(self):
-            self.obj.click_cou()
-            sleep(1)
-            d(scrollable=True).scroll.toEnd(10)
-            sleep(2)
-        @allure.step(title="操作关卡内部")
-        def test_005(self):
-            self.obj.click_for_cou()
-        def test_008(self):
-            self.obj.click_reivw()
-            self.obj.click_go_reviw()
-            self.obj.back()
-        def test_009(self):
-            self.obj.click_foud()
-            self.obj.click_xiaodu()
-            self.obj.click_my()
-            self.obj.click_seting()
-            self.obj.click_add_login()
+    @allure.step(title="点击微信登陆")
+    def test_001(self):
+        self.obj.click_wx_lojin()
+    @allure.step(title="点击如何上课")
+    def test_002(self):
+        self.obj.click_who()
+        self.obj.click_who_add()
+    @allure.step(title="点击购买课程")
+    def test_007(self):
+        sleep(1)
+        self.obj.click_buy_coiu()
+    @allure.step(title="点击课堂")
+    def test_003(self):
+        self.obj.click_cou()
+        sleep(1)
+        d(scrollable=True).scroll.toEnd(10)
+        sleep(2)
+    @allure.step(title="操作关卡内部")
+    def test_005(self):
+        self.obj.click_for_cou()
+
+    @allure.step(title="进入reivw")
+    def test_008(self):
+        self.obj.click_reivw()
+        self.obj.click_go_reviw()
+        self.obj.back()
+
+    @allure.step(title="退出账号")
+    def test_009(self):
+        self.obj.click_foud()
+        self.obj.click_xiaodu()
+        self.obj.click_my()
+        self.obj.click_seting()
+        self.obj.click_add_login()
