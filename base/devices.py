8f07ec388400cc80fab21b8e4f4b66d3c7c0737c
niuqun1 <1103126307@qq.com>
2019-09-12 12:08:46 +0800

    修复缺少参数

--- base/devices.py
+++ base/devices.py
@@ -13,7 +13,7 @@
 """
 import subprocess
 class get_devices:
-    def __init__(self, d):
+    def __init__(self,d):
         #初始化driver -- 供find_element 和 find_elements使用
         self.driver = d
     def search_element(self,loc,values=None):
@@ -23,7 +23,8 @@ class get_devices:
             return self.driver(xpath=values)
         elif loc == "t":
             return  self.driver(text=values)
-        else:print("输入错误")
+        else:
+            print("输入错误")
     def click_search(self,loc,values):
         # 点击事件
         """
@@ -48,18 +49,6 @@ class get_devices:
         :return:
         """
         self.driver.screenshot(name)
-    # 获取设备id
-    def getphonelist():
-        cmd = r'adb logcat >/Users/edz/Desktop/log/1.txt'  # % apk_file
-        pr = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
-        # pr.wait()  # 不会马上返回输出的命令，需要等待
-        out = pr.stdout.readlines()  # out = pr.stdout.read().decode("UTF-8")
-        # devices = []
-        # for i in (out)[1:-1]:
-        #     device = str(i).split("\\")[0].split("'")[-1]
-        #     devices.append(device)
-        # return devices  # 手机设备列表
-    # print(getphonelist())
     def slide(self,startx, starty, endx, endy, duration=None ):
         """
 
